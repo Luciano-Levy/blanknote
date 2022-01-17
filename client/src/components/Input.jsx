9f6e6672123b3797ec4aef292855841306d10a01
Luciano Levy <lucianolevy03@gmail.com>
2022-01-17 16:30:11 -0300

    input placeholder

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import {React} from 'react';
 
 export default function Input(props){
+
+  //como funciona el placeholder con el servidor
+
   return(
     <>
-      <textarea className='txtInput' spellCheck='true' placeholder={(props.txt != '')  ?  'Escribi lo que quieras!!' : props.txt} onBlur={props.changeInpt} style={{display: props.shift ? 'block' : 'none'}} defaultValue={props.txt}></textarea>
+      <textarea className='txtInput' spellCheck='true' placeholder='Escribi lo que quieras!!' onBlur={props.changeInpt} style={{display: props.shift ? 'block' : 'none'}} defaultValue={props.txt}></textarea>
     </>
   );
 };
